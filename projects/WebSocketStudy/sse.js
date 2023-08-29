463c966ebf20a293cd1a412f4358dd5bb9e970ed
闰土 <413365742@qq.com>
2023-08-29 21:03:56 +0800

    实现简单的sse

---
+++ projects/WebSocketStudy/sse.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'));
+
+app.get('/subscribe', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+     Connection: 'keep-alive',
+    'Cache-Control': 'no-cache',
+  });
+
+  let counter = 0;
+
+  // Send a message on connection
+  res.write('event: connected\n');
+  res.write(`data: You are now subscribed!\n`);
+  res.write(`id: ${counter}\n\n`);
+  counter += 1;
+
+  // Send a subsequent message every five seconds
+  setInterval(() => {
+    res.write('event: message\n');
+    res.write(`data: ${new Date().toLocaleString()}\n`);
+    res.write(`id: ${counter}\n\n`);
+    counter += 1;
+  }, 5000);
+
+  // Close the connection when the client disconnects
+  req.on('close', () => res.end('OK'))
+});
+
+app.listen(3000, () => {
+    console.log('server start')
+})
